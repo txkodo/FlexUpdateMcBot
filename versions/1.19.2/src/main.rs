fddf90c60e85c0aaac48d28ccd72d89c4f3c10e2
txkodo <kodo.hsmkai@gmail.com>
2025-07-09 23:04:41 +0900

    1.19.2の構成を整理

--- versions/1.19.2/src/main.rs
+++ versions/1.19.2/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use azalea_client::{Account, Client};
+use azalea_protocol::ServerAddress;
+use std::env;
+
+struct Args {
+    username: String,
+    host: String,
+    port: u16,
+}
+
+impl Args {
+    fn parse() -> Result<Self> {
+        let args: Vec<String> = env::args().collect();
+
+        let port = args[3]
+            .parse::<u16>()
+            .map_err(|_| anyhow::anyhow!("Invalid port number: {}", args[3]))?;
+
+        Ok(Args {
+            username: args[1].clone(),
+            host: args[2].clone(),
+            port
+        })
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse()?;
+
+    let (_client, mut event) = Client::join(
+        &Account::offline(&args.username),
+        ServerAddress {
+            host: args.host,
+            port: args.port,
+        },
+    )
+    .await?;
+
+    while let Some(e) = event.recv().await {
+        match e {
+            azalea_client::Event::Login => {
+                println!(r#"{{"event":"login"}}"#);
+            }
+            _ => {}
+        }
+    }
+    Ok(())
 }
