640f3e2ebb9f1bef7af07d3345d716fdf231009e
GitHub Action <action@github.com>
2025-09-30 18:09:06 +0000

    Updated to 643fcb98 (MC 1.21.9)

---
+++ versions/1.21.9/src/main.rs
@@ -0,0 +1,54 @@
+use anyhow::Result;
+use azalea_client::{Account, Client, Event};
+use azalea_protocol::{
+    ServerAddress,
+    packets::game::ClientboundGamePacket,
+};
+use common::{StdoutEvent, serialize_stdout_line};
+use std::io::{self, Write};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = common::parse_args();
+
+    let (_client, mut event) = Client::join(
+        Account::offline(&args.username),
+        ServerAddress {
+            host: args.host,
+            port: args.port,
+        },
+    )
+    .await?;
+
+    let mut stdout = io::stdout();
+
+    while let Some(e) = event.recv().await {
+        match e {
+            Event::Spawn => {
+                stdout.write(&serialize_stdout_line(&StdoutEvent::Spawn {}))?;
+                stdout.write("\n".as_bytes())?;
+            }
+            Event::Disconnect(reason) => {
+                stdout.write(&serialize_stdout_line(&StdoutEvent::Disconnect {
+                    reason: reason
+                        .map(|x| x.to_string())
+                        .unwrap_or("unknown".to_string()),
+                }))?;
+                stdout.write("\n".as_bytes())?;
+                break;
+            }
+            Event::Packet(packet) => match &*packet {
+                ClientboundGamePacket::LevelChunkWithLight(packet) => {
+                    stdout.write(&serialize_stdout_line(&StdoutEvent::Chunk {
+                        x: packet.x,
+                        z: packet.z,
+                    }))?;
+                    stdout.write("\n".as_bytes())?;
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+    Ok(())
+}
